a400b8b59725c99492e09ed3044c270c3fc82130
eduarde <eduard.erja@gmail.com>
2022-06-14 20:15:51 +0300

    Publish code for restaurant reviews

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
 # reviews_helper
+
+
+# BikesInStock
+
+
+Prerequisites:
+
+1. Install python locally (ex: 3.7.3).
+2. Create a Lambda function on AWS.
+   Choose python 3.7 as Runtime and choose 'app.lambda_function.lambda_handler' as handler.
+   Remember the name of your lambda, it will used in step 7.
+
+3. Create a variables.py file. Take variables_example.py as example. 
+
+
+Steps:
+
+1. Install a virtualenv lib via:
+```
+ pip install virtualenv
+```
+
+2. Navigate to the root of this project.
+3. Create a virtualenv (venv) via command:
+```
+virtualenv venv
+```
+4. Activate the virtualenv: 
+
+linux:
+```
+source venv/bin/activate
+```
+
+win:
+```
+venv/Scripts/activate
+```
+
+5. Install requirements.
+```
+pip install -r requirments.txt
+```
+
+6. Prepare the deployment package for aws.
+Readme: https://docs.aws.amazon.com/lambda/latest/dg/python-package.html
+
+In aws documentation they specify that you have to deactivate the venv.
+```
+deactivate
+```
+
+```
+cd venv/lib/python3.7/site-packages
+zip -r ../../../../my-deployment-package.zip .
+```
+
+7. Deploy and Update. (whenever you update the code). Navigate to the root of the project and run the following commands:
+```
+zip -g my-deployment-package.zip app/__init__.py app/lambda_function.py app/variables.py
+aws lambda update-function-code --function-name generate_review --zip-file fileb://my-deployment-package.zip
+```
---
+++ app/lambda_function.py
@@ -0,0 +1,56 @@
+import json
+import openai
+from variables import OPENAI_API_KEY
+
+
+def generate_review(api: openai, prompt: str): 
+    """ AI generates a review based on prompt """
+
+    review_text = ""
+    try:
+        response = api.Completion.create(
+            model="text-davinci-002",
+            prompt=prompt,
+            temperature=0.5,
+            max_tokens=64,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+        review_text = response['choices'][0]['text']
+    except (IndexError, AttributeError): 
+        print("Error in retrieving the review message.")
+
+    return review_text
+
+
+def create_prompt_message(tagline: str, name: str, tags: str):
+    """ Generate the prompt needed for AI """ 
+
+    return f"{tagline}:\n\nName: {name}\n{', '.join(tags)}\n\nReview:"
+
+def lambda_handler(event, context):
+    """ Handle the review generation via OpenAI
+
+    Example:
+
+        tagline = "Write a restaurant review based on these notes"
+        name = "Pizza & Pasta Bar"
+        tags = ["pizza good", "nice atmosphere", "cozy place", "long waiting"]
+    
+    """
+
+
+    
+    openai.api_key = OPENAI_API_KEY
+    prompt = create_prompt_message(
+        tagline=event['tagline'],
+        name=event['name'],
+        tags=event['tags']
+    )
+    text = generate_review(openai, prompt)
+   
+    return {
+        'statusCode': 200,
+        'body': json.dumps(text)
+    }
